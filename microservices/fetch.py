807e76c2b8f1a9f2816968f4cab7a8832ea9f8a1
Sergey Skripnick <sskripnick@mirantis.com>
2016-06-07 14:58:27 +0300

    Do not hide git fetch errors
    Change-Id: I430b03b42e44a691992c90c0448808520235e2c7

--- microservices/fetch.py
+++ microservices/fetch.py
@@ -14,14 +14,21 @@ LOG = logging.getLogger(__name__)
 
 
 def fetch_repository(component):
-    dest_dir = os.path.join(CONF.repositories.path, component)
-    if os.path.isdir(dest_dir):
-        LOG.info('%s was already cloned, skipping', component)
-        return
-    git_url = getattr(CONF.repositories, component.replace('-', '_')) % \
-        CONF.auth.gerrit_username
-    git.Repo.clone_from(git_url, dest_dir)
-    LOG.info('Cloned %s repo', component)
+    try:
+        dest_dir = os.path.join(CONF.repositories.path, component)
+        if os.path.isdir(dest_dir):
+            LOG.info('%s was already cloned, skipping', component)
+            return
+        git_url = getattr(CONF.repositories, component.replace('-', '_')) % \
+            CONF.auth.gerrit_username
+        git.Repo.clone_from(git_url, dest_dir)
+        LOG.info('Cloned %s repo', component)
+    except (git.exc.GitCommandError,
+            git.exc.HookExecutionError,
+            git.exc.CheckoutError) as ex:
+        # NOTE(sskripnick) multiprocessing.Pool can't handle some git
+        # exceptions properly
+        raise Exception("%s" % ex)
 
 
 def fetch_repositories(components=None):
@@ -34,5 +41,13 @@ def fetch_repositories(components=None):
 
     tasks = [pool.apply_async(fetch_repository, (component,))
              for component in components]
+    errors = 0
     for task in tasks:
         task.wait()
+        try:
+            task.get()
+        except Exception as ex:
+            LOG.error("Failed to fetch: %s" % ex)
+            errors += 1
+    if errors:
+        raise Exception("Failed to fetch %d repos" % errors)
