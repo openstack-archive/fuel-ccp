e98bc37c0cfb58883d8862aaa859a05dde732191
Dmitriy Ukhlov <dukhlov@mirantis.com>
2016-08-05 11:34:49 +0300

    Adds validation for role object
    execute "fuel-ccp validate {type,}" to validate specified types
    for component repositories. If type list is not specified - validation
    of all available types will be performed.
    
    This patch supports only validation for "service_def" type.
    
    Also service validation is added before each deploy action.
    
    Change-Id: Icf34c722ba441537b213671f93f9d322d3a8325a

---
+++ fuel_ccp/validate.py
@@ -0,0 +1,18 @@
+from fuel_ccp.common import utils
+
+from validation import service as service_validation
+
+
+def validate(components, types):
+    if not types:
+        types = ["service_def"]
+
+    for validation_type in set(types):
+        if validation_type == "service_def":
+            component_map = utils.get_deploy_components_info()
+            service_validation.validate_service_definitions(component_map,
+                                                            components)
+        else:
+            raise RuntimeError(
+                "Unexpected validation type: '{}'".format(validation_type)
+            )
