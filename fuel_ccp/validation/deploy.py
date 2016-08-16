ac8f3ec3ab875020b31a8e5101f2328ee75aa85d
Andrey <apavlov@mirantis.com>
2016-08-16 07:03:44 +0000

    Adding validation for per-service deploy
    * check that components names are correct
    * check that requested componets are not deployed
    * check that all required components already exist/requested
    
    Change-Id: I110eb8cdb8c450e9fd6b6cd8726a27bf1a7d4ac8

---
+++ fuel_ccp/validation/deploy.py
@@ -0,0 +1,24 @@
+from fuel_ccp.common import utils
+from fuel_ccp import dependencies
+
+
+def validate_requested_components(components, components_map):
+    """Validate requested components.
+
+    Validate that requested components are not already deployed and all
+    required components provided.
+    """
+    deployed_components = utils.get_deployed_components()
+    required_components = dependencies.get_deps(components, components_map)
+
+    already_deployed_components = components & deployed_components
+    if already_deployed_components:
+        raise RuntimeError('Following components are already deployed: '
+                           '%s' % ' '.join(already_deployed_components))
+
+    not_provided_components = (required_components - components -
+                               deployed_components)
+    if not_provided_components:
+        raise RuntimeError('Following components are also required for '
+                           'successful deployment: '
+                           '%s' % ' '.join(not_provided_components))
