d906f039d2b964c7a058787046f7cbb48748d228
Andrey <apavlov@mirantis.com>
2016-08-16 07:03:44 +0000

    Adding validation for per-service deploy
    * check that components names are correct
    * check that requested components are not deployed
    * check that all required components already exist/requested
    
    Change-Id: I110eb8cdb8c450e9fd6b6cd8726a27bf1a7d4ac8

---
+++ fuel_ccp/validation/base_validation.py
@@ -0,0 +1,6 @@
+def validate_components_names(components, components_map):
+    valid_components = set(components_map.keys())
+    invalid_components = components - valid_components
+    if invalid_components:
+        raise RuntimeError('Following components do not match any '
+                           'definitions: %s' % ' '.join(invalid_components))
