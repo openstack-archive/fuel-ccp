d559019b16ec0e365f0f286d5361fff7a5c17a72
Andrey Pavlov <apavlov@mirantis.com>
2016-08-18 17:21:45 +0300

    Per-service deployment implementation
    Changing per-repository deployment to
    per-service
    
    This change affects "ccp deploy" command:
    now -c/--components argument takes services
    names instead of repositories names
    
    Change-Id: I236bb957a7b93e21ed05fec9b1cab2a27d87d5f4

--- fuel_ccp/common/utils.py
+++ fuel_ccp/common/utils.py
@@ -1,5 +1,6 @@
 import os
 import pkg_resources
+import re
 
 from oslo_config import cfg
 from oslo_log import log as logging
@@ -52,3 +53,26 @@ def get_global_parameters(*config_groups):
             LOG.debug("\"%s\" not found, skipping", path)
 
     return cfg
+
+
+def get_deploy_components_info():
+    yaml_file_re = re.compile(r'\.yaml$')
+    components_map = {}
+
+    for component in CONF.repositories.names:
+        service_dir = os.path.join(CONF.repositories.path,
+                                   component,
+                                   'service')
+        if not os.path.isdir(service_dir):
+            continue
+        for service_file in os.listdir(service_dir):
+            if yaml_file_re.search(service_file):
+                LOG.debug("Parse service definition: %s", service_file)
+                with open(os.path.join(service_dir, service_file), "r") as f:
+                    service_definition = yaml.load(f)
+                service_name = service_definition['service']['name']
+                components_map[service_name] = {
+                    'service_dir': service_dir,
+                    'service_content': service_definition
+                }
+    return components_map
