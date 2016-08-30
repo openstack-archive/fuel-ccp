988c3047b4bbdd0670ab631c72f80a9e720f9df0
Proskurin Kirill <kproskurin@mirantis.com>
2016-08-30 15:24:45 +0300

    Update docs general info a bit
    Change-Id: Ia2f8bbba0daf73cea424aa70db6d35c18834747c

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,8 +37,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'microservices'
-copyright = u'2013, OpenStack Foundation'
+project = u'CCP'
+copyright = u'2016, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
