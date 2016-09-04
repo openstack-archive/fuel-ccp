d2128935d9acd5c4229029ead299feaaec2a521b
Andrey <apavlov@mirantis.com>
2016-09-04 15:58:24 +0000

    Migrationg to pykube from python-k8sclient
    Config changes:
    * new `insecure` flag in `kubernetes` group
    * `ca-certs` -> `ca-cert`
    
    Change-Id: I4dd91828e7276dc781baea4c6df2c01b2f1312e0

--- fuel_ccp/kubernetes.py
+++ fuel_ccp/kubernetes.py
@@ -1,15 +1,10 @@
 import logging
 import os
+
+import pykube
 import yaml
 
-from k8sclient.client import api_client
-from k8sclient.client.apis import apisbatchv_api
-from k8sclient.client.apis import apisextensionsvbeta_api
-from k8sclient.client.apis import apiv_api
-import k8sclient.client.rest
-
 from fuel_ccp import config
-
 
 CONF = config.CONF
 
@@ -17,108 +12,116 @@ LOG = logging.getLogger(__name__)
 
 
 def get_client(kube_apiserver=None, key_file=None, cert_file=None,
-               ca_certs=None):
+               ca_cert=None, insecure=None):
     kube_apiserver = kube_apiserver or CONF.kubernetes.server
     key_file = key_file or CONF.kubernetes.key_file
     cert_file = cert_file or CONF.kubernetes.cert_file
-    ca_certs = ca_certs or CONF.kubernetes.ca_certs
+    ca_cert = ca_cert or CONF.kubernetes.ca_cert
+    insecure = insecure or CONF.kubernetes.insecure
 
-    return api_client.ApiClient(host=kube_apiserver, key_file=key_file,
-                                cert_file=cert_file, ca_certs=ca_certs)
+    cluster = {"server": kube_apiserver}
+    if ca_cert:
+        cluster["certificate-authority"] = ca_cert
+    elif insecure:
+        cluster['insecure-skip-tls-verify'] = insecure
+
+    user = {}
+    if cert_file and key_file:
+        user["client-certificate"] = cert_file
+        user["client-key"] = key_file
+
+    config = {
+        "clusters": [
+            {
+                "name": "ccp",
+                "cluster": cluster
+            }
+        ],
+        "users": [
+            {
+                "name": "ccp",
+                "user": user
+            }
+        ],
+        "contexts": [
+            {
+                "name": "ccp",
+                "context": {
+                    "cluster": "ccp",
+                    "user": "ccp"
+                },
+            }
+        ],
+        "current-context": "ccp"
+    }
+    return pykube.HTTPClient(pykube.KubeConfig(config))
+
+
+def export_object(object_dict):
+    file_name = '%s-%s.yaml' % (
+        object_dict['metadata']['name'], object_dict['kind'].lower())
+    if object_dict['kind'] == 'ConfigMap':
+        file_path = os.path.join(
+            CONF.action.export_dir, 'configmaps', file_name)
+    else:
+        file_path = os.path.join(CONF.action.export_dir, file_name)
+    with open(file_path, 'w') as object_file:
+        object_file.write(yaml.dump(
+            object_dict, default_flow_style=False))
 
 
 def create_object_from_definition(object_dict, namespace=None, client=None):
     LOG.debug("Deploying %s: \"%s\"",
               object_dict["kind"], object_dict["metadata"]["name"])
-    if CONF.action.export_dir:
-        file_name = '%s-%s.yaml' % (
-            object_dict['metadata']['name'], object_dict['kind'].lower())
-        if object_dict['kind'] == 'ConfigMap':
-            file_path = os.path.join(
-                CONF.action.export_dir, 'configmaps', file_name)
-        else:
-            file_path = os.path.join(CONF.action.export_dir, file_name)
-        with open(file_path, 'w') as object_file:
-            object_file.write(yaml.dump(
-                object_dict, default_flow_style=False))
+    if not object_dict['kind'] == 'Namespace':
+        if CONF.action.export_dir:
+            export_object(object_dict)
+        if CONF.action.dry_run:
+            LOG.info(yaml.dump(object_dict, default_flow_style=False))
+            return
 
-    if CONF.action.dry_run:
-        LOG.info(yaml.dump(object_dict, default_flow_style=False))
-        return
+        object_dict['metadata']['namespace'] = (
+            namespace or CONF.kubernetes.namespace)
 
-    namespace = namespace or CONF.kubernetes.namespace
-    client = client or get_client()
-    if object_dict['kind'] == 'Deployment':
-        api = apisextensionsvbeta_api.ApisextensionsvbetaApi(client)
-        resp = api.create_namespaced_deployment(
-            body=object_dict, namespace=namespace)
-    elif object_dict['kind'] == 'DaemonSet':
-        api = apisextensionsvbeta_api.ApisextensionsvbetaApi(client)
-        resp = api.create_namespaced_daemon_set(
-            body=object_dict, namespace=namespace)
-    elif object_dict['kind'] == 'Service':
-        api = apiv_api.ApivApi(client)
-        resp = api.create_namespaced_service(
-            body=object_dict, namespace=namespace)
-    elif object_dict['kind'] == 'Pod':
-        api = apiv_api.ApivApi(client)
-        resp = api.create_namespaced_pod(
-            body=object_dict, namespace=namespace)
-    elif object_dict["kind"] == "Job":
-        api = apisbatchv_api.ApisbatchvApi(client)
-        resp = api.create_namespaced_job(
-            body=object_dict, namespace=namespace)
-    elif object_dict["kind"] == "ConfigMap":
-        api = apiv_api.ApivApi(client)
-        resp = api.create_namespaced_config_map(
-            body=object_dict, namespace=namespace)
-    else:
+    obj_class = getattr(pykube, object_dict["kind"], None)
+    if not obj_class:
         LOG.warning('"%s" object is not supported, skipping.'
                     % object_dict['kind'])
         return
 
-    LOG.debug('%s "%s" has been created' % (
-        object_dict['kind'], object_dict['metadata']['name']))
-    return resp
-
-
-def get_v1_api(client):
-    return apiv_api.ApivApi(client)
+    client = client or get_client()
+    obj = obj_class(client, object_dict)
+    if obj.exists():
+        LOG.debug('%s "%s" already exists', object_dict['kind'],
+                  object_dict['metadata']['name'])
+        return obj
+    obj.create()
+    LOG.debug('%s "%s" has been created', object_dict['kind'],
+              object_dict['metadata']['name'])
+    return obj
 
 
 def list_k8s_nodes():
-    api = get_v1_api(get_client())
-    return api.list_namespaced_node().items
+    client = get_client()
+    return pykube.Node.objects(client).all()
 
 
 def list_cluster_daemonsets():
     client = get_client()
-    api = apisextensionsvbeta_api.ApisextensionsvbetaApi(client)
-    return api.list_namespaced_daemon_set(
+    return pykube.DaemonSet.objects(client).filter(
         namespace=CONF.kubernetes.namespace,
-        label_selector="ccp=true").items
+        selector="ccp=true")
 
 
 def list_cluster_deployments():
     client = get_client()
-    api = apisextensionsvbeta_api.ApisextensionsvbetaApi(client)
-    return api.list_namespaced_deployment(
+    return pykube.Deployment.objects(client).filter(
         namespace=CONF.kubernetes.namespace,
-        label_selector="ccp=true").items
+        selector="ccp=true")
 
 
 def get_object_names(items):
     names = []
     for item in items:
-        names.append(item.metadata.name)
+        names.append(item.name)
     return names
-
-
-def handle_exists(fct, *args, **kwargs):
-    try:
-        fct(*args, **kwargs)
-    except k8sclient.client.rest.ApiException as e:
-        if e.status == 409:
-            LOG.debug("Resource exists")
-        else:
-            raise e
