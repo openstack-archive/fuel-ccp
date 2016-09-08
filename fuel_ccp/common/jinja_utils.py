bee2790e56d64506eee07cd9eed5cdb47e6824da
Dmitriy Ukhlov <dukhlov@mirantis.com>
2016-09-08 14:49:54 +0300

    Add rendering of whole service.yaml file before parsing
    Change-Id: I3b5f3d0ac5ac9180bdb32029faf56b0834c6e4bc

--- fuel_ccp/common/jinja_utils.py
+++ fuel_ccp/common/jinja_utils.py
@@ -9,15 +9,9 @@ def str_to_bool(text):
 
 def jinja_render(path, context, functions=()):
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        os.path.dirname(path)))
+        os.path.dirname(path)), undefined=jinja2.StrictUndefined)
     env.filters['bool'] = str_to_bool
     for func in functions:
         env.globals[func.__name__] = func
     content = env.get_template(os.path.basename(path)).render(context)
     return content
-
-
-def jinja_render_str(content, jvars):
-    env = jinja2.Environment(loader=jinja2.DictLoader({'default': content}))
-    env.filters['bool'] = str_to_bool
-    return env.get_template('default').render(jvars)
---
+++ fuel_ccp/tests/common/test_utils.py
@@ -0,0 +1,114 @@
+import os
+
+from jinja2 import exceptions as jinja_exceptions
+from mock import mock
+import yaml
+
+from fuel_ccp.common import utils
+from fuel_ccp.tests import base
+
+
+class TestUtils(base.TestCase):
+
+    @mock.patch("fuel_ccp.common.utils.get_global_parameters")
+    def test_get_deploy_components_info_with_default_context(
+            self, get_global_parameters_mock):
+
+        default_params = {
+            "configs": {
+                "db_root_password": "db_root_password_default",
+                "keystone_db_name": "keystone_db_name_default",
+                "keystone_db_username": "keystone_db_username_default",
+                "keystone_db_password": "keystone_db_password_default",
+                "openstack_user_password": "os_user_password_default",
+                "openstack_user_name": "os_user_name_default",
+                "openstack_project_name": "os_project_name_default",
+                "openstack_role_name": "os_role_name_default",
+                "keystone_admin_port": "keystone_admin_port_default",
+                "keystone_public_port": "keystone_public_port_default"
+            }
+        }
+
+        get_global_parameters_mock.return_value = default_params
+
+        base_dir = os.path.dirname(__file__)
+
+        self.conf.repositories.path = os.path.join(base_dir, "test_repo_dir")
+        self.conf.repositories.names = ["component"]
+
+        res = (
+            utils.get_deploy_components_info()["component"]["service_content"]
+        )
+
+        get_global_parameters_mock.assert_called_once_with("configs")
+
+        with open(os.path.join(base_dir,
+                               "service-rendered-example-default.yaml")) as f:
+            expected = yaml.load(f)
+
+        self.assertDictEqual(expected, res)
+
+    @mock.patch("fuel_ccp.common.utils.get_global_parameters")
+    def test_get_deploy_components_info_with_custom_context(
+            self, get_global_parameters_mock):
+
+        custom_params = {
+            "configs": {
+                "db_root_password": "db_root_password_custom",
+                "keystone_db_name": "keystone_db_name_custom",
+                "keystone_db_username": "keystone_db_username_custom",
+                "keystone_db_password": "keystone_db_password_custom",
+                "openstack_user_password": "os_user_password_custom",
+                "openstack_user_name": "os_user_name_custom",
+                "openstack_project_name": "os_project_name_custom",
+                "openstack_role_name": "os_role_name_custom",
+                "keystone_admin_port": "keystone_admin_port_custom",
+                "keystone_public_port": "keystone_public_port_custom"
+            }
+        }
+
+        base_dir = os.path.dirname(__file__)
+
+        self.conf.repositories.path = os.path.join(base_dir, "test_repo_dir")
+        self.conf.repositories.names = ["component"]
+
+        res = utils.get_deploy_components_info(
+            rendering_context=custom_params["configs"]
+        )["component"]["service_content"]
+
+        get_global_parameters_mock.assert_not_called()
+
+        with open(os.path.join(base_dir,
+                               "service-rendered-example-custom.yaml")) as f:
+            expected = yaml.load(f)
+
+        self.assertDictEqual(expected, res)
+
+    @mock.patch("fuel_ccp.common.utils.get_global_parameters")
+    def test_get_deploy_components_info_with_not_enough_context(
+            self, get_global_parameters_mock):
+
+        default_params = {
+            "configs": {
+                "db_root_password": "db_root_password_default",
+                "keystone_db_name": "keystone_db_name_default",
+                "keystone_db_username": "keystone_db_username_default",
+                "keystone_db_password": "keystone_db_password_default",
+                "openstack_user_password": "os_user_password_default",
+                "openstack_user_name": "os_user_name_default",
+                "openstack_project_name": "os_project_name_default",
+                "openstack_role_name": "os_role_name_default",
+            }
+        }
+
+        get_global_parameters_mock.return_value = default_params
+
+        base_dir = os.path.dirname(__file__)
+
+        self.conf.repositories.path = os.path.join(base_dir, "test_repo_dir")
+        self.conf.repositories.names = ["component"]
+
+        self.assertRaises(jinja_exceptions.UndefinedError,
+                          utils.get_deploy_components_info)
+
+        get_global_parameters_mock.assert_called_once_with("configs")
