a57e53ee95a51ce2cdcf396762cd8dddbbb37772
Sergey Reshetnyak <sreshetniak@mirantis.com>
2016-09-12 22:14:11 +0300

    Deploy registry on first node in k8s
    Also use local directory for registry storage. It allows use builded
    images after redeploy registry.
    
    Change-Id: Ia3ffc04275dc675dc9863decb7ced20d890473a1

--- tools/registry/deploy-registry.sh
+++ tools/registry/deploy-registry.sh
@@ -32,6 +32,9 @@ function kube_cmd {
     kubectl $SRV_OPT $NAMESPACE_OPT "$@"
 }
 
+first_node=$(kubectl get nodes -o template --template="{{ with index .items 0 }}{{ .metadata.name }}{{ end }}")
+kubectl label node $first_node app=ccp-registry
+
 workdir=$(dirname $0)
 
 kube_cmd create -f $workdir/registry-pod.yaml
