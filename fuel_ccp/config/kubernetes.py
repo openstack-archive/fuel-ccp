69e2aa909c639b1ce683eddf9a9b29534a53ee88
Yuriy Taraday <yorik.sar@gmail.com>
2016-09-14 20:37:54 +0300

    [BREAKING CLI] Remove oslo.config and oslo.log
    This change replaces oslo.config CLI parsing with dumb parser with
    arguments that will be supplorted by cliff and doesn't provide
    alternative for oslo.log, so logging will be broken until next change
    with cliff lands. Add dumb logging.basicConfig until then.
    
    Depends-On: Ibafdaf073bf973f05a721f8422e48e1f581dc4a4
    Change-Id: I82169d36021201964a48d7e35161c51e10f30f4a

--- fuel_ccp/config/kubernetes.py
+++ fuel_ccp/config/kubernetes.py
@@ -1,27 +1,3 @@
-from oslo_config import cfg
-
-
-CONF = cfg.CONF
-kubernetes_opts = [
-    cfg.StrOpt('server',
-               default='127.0.0.1:8080',
-               help='Addres and port for kube-apiserver'),
-    cfg.StrOpt('namespace',
-               default='ccp',
-               help='The name of the namespace'),
-    cfg.StrOpt('ca-certs',
-               help='The location of the CA certificate files'),
-    cfg.StrOpt('key-file',
-               help='The location of the key file'),
-    cfg.StrOpt('cert-file',
-               help='The location of the certificate file')
-]
-kubernetes_opt_group = cfg.OptGroup(name='kubernetes',
-                                    title='Kubernetes client')
-CONF.register_group(kubernetes_opt_group)
-CONF.register_cli_opts(kubernetes_opts, kubernetes_opt_group)
-CONF.register_cli_opts(kubernetes_opts, kubernetes_opt_group)
-
 DEFAULTS = {
     'kubernetes': {
         'server': '127.0.0.1:8080',
--- fuel_ccp/tests/test_config.py
+++ fuel_ccp/tests/test_config.py
@@ -1,9 +1,5 @@
-import collections
-import functools
-
 import fixtures
 import jsonschema
-from oslo_config import cfg
 import six
 import testscenarios
 
@@ -38,9 +34,7 @@ class TestGetCLIConfig(testscenarios.WithScenarios, base.TestCase):
     def test_get_cli_config(self):
         self.useFixture(fixtures.MockPatch(
             'argparse.ArgumentParser.error', side_effect=ArgumentParserError))
-        new_argv = ['ccp'] + self.argv
-        self.useFixture(fixtures.MockPatch('sys.argv', new=new_argv))
-        result = config.get_cli_config()
+        result = config.get_cli_config(self.argv)
         self.assertEqual(result, self.expected_result)
 
 
@@ -52,92 +46,6 @@ def nested_dict_to_attrdict(d):
         return list(map(nested_dict_to_attrdict, d))
     else:
         return d
-
-
-class TestSetOsloDefaults(testscenarios.WithScenarios, base.TestCase):
-    scenarios = [
-        ('empty', {'yconf': {}, 'expected_defaults': {}}),
-        ('bogus', {'yconf': {'bogus': 1}, 'expected_defaults': {}}),
-        ('simple', {
-            'yconf': {'debug': False},
-            'expected_defaults': {None: {'debug': False}},
-        }),
-        ('deep', {
-            'yconf': {'thegroup': {'count': 42}},
-            'expected_defaults': {'thegroup': {'count': 42}},
-        }),
-    ]
-
-    yconf = None
-    expected_defaults = None
-
-    def setUp(self):
-        super(TestSetOsloDefaults, self).setUp()
-        self.conf = cfg.ConfigOpts()
-        self.conf.register_opt(cfg.BoolOpt('debug', default=False))
-        self.conf.register_opt(cfg.IntOpt('count'), group='thegroup')
-
-    def get_defaults(self):
-        res = collections.defaultdict(
-            functools.partial(collections.defaultdict, dict))
-        for opt_info, group in self.conf._all_opt_infos():
-            try:
-                default = opt_info['default']
-            except KeyError:
-                continue
-            if group is not None:
-                group = group.name
-            res[group][opt_info['opt'].name] = default
-        return res
-
-    def test_set_oslo_defaults(self):
-        yconf = nested_dict_to_attrdict(self.yconf)
-        config.set_oslo_defaults(self.conf, yconf)
-        self.assertEqual(self.get_defaults(), self.expected_defaults)
-
-
-class TestCopyValuesFromOslo(testscenarios.WithScenarios, base.TestCase):
-    scenarios = [
-        ('simple', {
-            'yconf': {},
-            'oconf': {None: {'debug': True}},
-            'expected_result': {'debug': True, 'thegroup': {'count': None}},
-        }),
-        ('overwrite', {
-            'yconf': {'debug': False},
-            'oconf': {None: {'debug': True}},
-            'expected_result': {'debug': True, 'thegroup': {'count': None}},
-        }),
-        ('deep', {
-            'yconf': {'debug': False},
-            'oconf': {'thegroup': {'count': 3}},
-            'expected_result': {'debug': False, 'thegroup': {'count': 3}},
-        }),
-        ('deep_overwrite_with_bogus', {
-            'yconf': {'thegroup': {'bogus': 'value'}, 'other': 1},
-            'oconf': {'thegroup': {'count': 3}},
-            'expected_result': {
-                'debug': False,
-                'thegroup': {'count': 3, 'bogus': 'value'},
-                'other': 1,
-            },
-        }),
-    ]
-
-    yconf = None
-    oconf = None
-    expected_result = None
-
-    def test_copy_values_from_oslo(self):
-        conf = cfg.ConfigOpts()
-        conf.register_opt(cfg.BoolOpt('debug', default=False))
-        conf.register_opt(cfg.IntOpt('count'), group='thegroup')
-        for group, values in six.iteritems(self.oconf):
-            for key, value in six.iteritems(values):
-                conf.set_default(group=group, name=key, default=value)
-        yconf = nested_dict_to_attrdict(self.yconf)
-        config.copy_values_from_oslo(conf, yconf)
-        self.assertEqual(yconf, self.expected_result)
 
 
 class TestConfigSchema(base.TestCase):
