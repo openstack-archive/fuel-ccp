b55209fa5863daf7b743a6159b3a420bc4f1f963
Yuriy Taraday <yorik.sar@gmail.com>
2016-09-14 20:37:54 +0300

    [BREAKING CLI] Remove oslo.config and oslo.log
    This change replaces oslo.config CLI parsing with dumb parser with
    arguments that will be supplorted by cliff and doesn't provide
    alternative for oslo.log, so logging will be broken until next change
    with cliff lands. Add dumb logging.basicConfig until then.
    
    Depends-On: Ibafdaf073bf973f05a721f8422e48e1f581dc4a4
    Change-Id: I82169d36021201964a48d7e35161c51e10f30f4a

--- fuel_ccp/config/builder.py
+++ fuel_ccp/config/builder.py
@@ -1,35 +1,5 @@
 import multiprocessing
 
-from oslo_config import cfg
-
-
-CONF = cfg.CONF
-builder_opts = [
-    cfg.IntOpt('workers',
-               default=multiprocessing.cpu_count(),
-               help='Number of workers which build docker images'),
-    cfg.BoolOpt('keep-image-tree-consistency',
-                default=True,
-                help='Rebuild all descendant images to keep consistent state '
-                     'of image tree'),
-    cfg.BoolOpt('build-base-images-if-not-exist',
-                default=True,
-                help='Make sure that all base images required for target '
-                     'image building are ready and prebuild them if they are '
-                     'not'),
-    cfg.BoolOpt('push',
-                default=False,
-                help='Push to the Docker registry'),
-    cfg.BoolOpt('no-cache',
-                default=False,
-                help='Dont use docker cache')
-]
-builder_opt_group = cfg.OptGroup(name='builder',
-                                 title='Images builder')
-CONF.register_group(builder_opt_group)
-CONF.register_cli_opts(builder_opts, builder_opt_group)
-CONF.register_opts(builder_opts, builder_opt_group)
-
 DEFAULTS = {
     'builder': {
         'workers': multiprocessing.cpu_count(),
