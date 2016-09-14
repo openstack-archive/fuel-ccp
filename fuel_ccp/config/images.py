c7e2944bc233cea97909452c873569b1454dc9f6
Yuriy Taraday <yorik.sar@gmail.com>
2016-09-14 20:37:54 +0300

    [BREAKING CLI] Remove oslo.config and oslo.log
    This change replaces oslo.config CLI parsing with dumb parser with
    arguments that will be supplorted by cliff and doesn't provide
    alternative for oslo.log, so logging will be broken until next change
    with cliff lands. Add dumb logging.basicConfig until then.
    
    Depends-On: Ibafdaf073bf973f05a721f8422e48e1f581dc4a4
    Change-Id: I82169d36021201964a48d7e35161c51e10f30f4a

--- fuel_ccp/config/images.py
+++ fuel_ccp/config/images.py
@@ -1,29 +1,3 @@
-from oslo_config import cfg
-
-
-CONF = cfg.CONF
-images_opts = [
-    cfg.StrOpt('namespace',
-               default='ccp',
-               help='Namespace for Docker images'),
-    cfg.StrOpt('tag',
-               default='latest',
-               help='Tag for Docker images'),
-    cfg.StrOpt('base-distro',
-               default='debian',
-               help='Base distribution and image'),
-    cfg.StrOpt('base-tag',
-               default='jessie',
-               help='Tag of the base image'),
-    cfg.StrOpt('maintainer',
-               default='MOS Microservices <mos-microservices@mirantis.com>')
-]
-images_opt_group = cfg.OptGroup(name='images',
-                                title='Docker images')
-CONF.register_group(images_opt_group)
-CONF.register_cli_opts(images_opts, images_opt_group)
-CONF.register_opts(images_opts, images_opt_group)
-
 DEFAULTS = {
     'images': {
         'namespace': 'ccp',
