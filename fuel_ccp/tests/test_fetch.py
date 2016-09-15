edc1befb62766162383d190bea253bc9b6529143
ydehtiarov <ydehtiarov@mirantis.com>
2016-09-15 16:02:02 +0300

    Fetch url compose tests
    Change-Id: I500cf98ff1c4642cdff3a9e8f16eee892e0f5c6e

--- fuel_ccp/tests/test_fetch.py
+++ fuel_ccp/tests/test_fetch.py
@@ -2,13 +2,63 @@ import os
 
 import fixtures
 import mock
+import sys
 
+import testscenarios
 from fuel_ccp import fetch
 from fuel_ccp.tests import base
 
 
 @mock.patch('git.Repo.clone_from')
-class TestFetch(base.TestCase):
+class TestFetch(testscenarios.WithScenarios, base.TestCase):
+    scenarios = [
+        ("default", {
+            "option": "default",
+            "delta" : "",
+            "url": "https://review.openstack.org:443/openstack/%s{}"}),
+        ("hostname", {
+            "option": "hostname",
+            "delta" : "host.name",
+            "url": "https://{}:443/openstack/%s"}),
+        ('username', {
+            "option": "username",
+            "delta" : "someuser",
+            "url": "https://{}@review.openstack.org:443/openstack/%s",
+        }),
+        ('port', {
+            "option": "port",
+            "delta" : "9999",
+            'url': "https://review.openstack.org:{}/openstack/%s",
+        }),
+        ('protocol', {
+            "option": "protocol",
+            "delta" : "ssh",
+            'url': "{}://review.openstack.org:443/openstack/%s",
+        }),
+        ('protocol', {
+            "option": "protocol",
+            "delta" : "http",
+            'url': "{}://review.openstack.org:443/openstack/%s",
+        }),
+        ('protocol', {
+            "option": "protocol",
+            "delta" : "git",
+            'url': "{}://review.openstack.org:443/openstack/%s",
+        }),
+        ('protocol', {
+            "option": "protocol",
+            "delta" : "https",
+            'url': "{}://review.openstack.org:443/openstack/%s",
+        }),
+        ('project', {
+            "option": "project",
+            "delta" : "",
+            'url': "https://review.openstack.org:443/{}/%s",
+        })
+    ]
+    url = None
+    option = None
+    delta = None
 
     def setUp(self):
         super(TestFetch, self).setUp()
@@ -21,7 +71,9 @@ class TestFetch(base.TestCase):
         os.mkdir(os.path.join(self.tmp_path, 'ms-openstack-base'))
 
     def test_fetch_default_repositories(self, m_clone):
-        # All repos except ms-openstack-base
+        if self.option is not 'default':
+            self.conf['repositories'][self.option] = self.delta
+        self.conf['repositories']['path'] = self.tmp_path
         components = ['fuel-ccp-debian-base',
                       'fuel-ccp-entrypoint',
                       'fuel-ccp-etcd',
@@ -35,10 +87,11 @@ class TestFetch(base.TestCase):
                       'fuel-ccp-rabbitmq',
                       'fuel-ccp-stacklight']
         expected_calls = [
-            mock.call('https://%s@review.openstack.org:443/openstack/%s' % (
-                '', component), os.path.join(self.tmp_path, component))
+            mock.call(
+                self.url.format(self.delta) % (component),
+                os.path.join(self.tmp_path, component))
             for component in components
-        ]
+            ]
         for component, expected_call in zip(components, expected_calls):
             fetch.fetch_repository(component)
             self.assertIn(expected_call, m_clone.call_args_list)
