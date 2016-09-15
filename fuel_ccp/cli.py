276773f1f4199649374092291de976d90b1905e6
Yuriy Taraday <yorik.sar@gmail.com>
2016-09-15 16:13:07 +0300

    Switch to cliff
    Added all commands and basic app for cliff, removed all
    legacy/compatibility stuff around CLI.
    
    Config file CLI argument and logging will be fixed in separate commit.
    
    Change-Id: Ib52b4c139c2ac9ea5afa073febcd764edced1464

--- fuel_ccp/cli.py
+++ fuel_ccp/cli.py
@@ -1,6 +1,14 @@
+import argparse
+import logging
+import os.path
 import signal
 import sys
 
+from cliff import app
+from cliff import command
+from cliff import commandmanager
+
+import fuel_ccp
 from fuel_ccp import build
 from fuel_ccp import cleanup
 from fuel_ccp import config
@@ -9,48 +17,164 @@ from fuel_ccp import deploy
 from fuel_ccp import fetch
 
 CONF = config.CONF
+LOG = logging.getLogger(__name__)
 
 
-def do_build():
-    if CONF.builder.push and not CONF.registry.address:
-        raise RuntimeError('No registry specified, cannot push')
-    if CONF.repositories.clone:
-        do_fetch()
-    build.build_components(components=CONF.action.components)
+class BaseCommand(command.Command):
+    def get_parser(self, *args, **kwargs):
+        parser = super(BaseCommand, self).get_parser(*args, **kwargs)
+        parser.set_defaults(**CONF.action._dict)
+        return parser
 
 
-def do_deploy():
-    if CONF.repositories.clone:
-        do_fetch()
-    deploy.deploy_components(components=CONF.action.components)
+class Build(BaseCommand):
+    """Build CCP docker images"""
+
+    def get_parser(self, *args, **kwargs):
+        parser = super(Build, self).get_parser(*args, **kwargs)
+        parser.add_argument('-c', '--components',
+                            nargs='+',
+                            help='CCP component to build')
+        return parser
+
+    def take_action(self, parsed_args):
+        if CONF.builder.push and not CONF.registry.address:
+            raise RuntimeError('No registry specified, cannot push')
+        if CONF.repositories.clone:
+            do_fetch()
+        build.build_components(components=parsed_args.components)
+
+
+class Deploy(BaseCommand):
+    """Deploy CCP"""
+
+    def get_parser(self, *args, **kwargs):
+        parser = super(Deploy, self).get_parser(*args, **kwargs)
+        parser.add_argument('-c', '--components',
+                            nargs='+',
+                            help='CCP component to build')
+        parser.add_argument("--dry-run",
+                            action='store_true',
+                            help="Print k8s objects definitions without"
+                                 "actual creation")
+        parser.add_argument('--export-dir',
+                            help='Directory to export created k8s objects')
+        return parser
+
+    def take_action(self, parsed_args):
+        if CONF.repositories.clone:
+            do_fetch()
+        # only these two are being implicitly passed
+        CONF.action._update(
+            dry_run=parsed_args.dry_run,
+            export_dir=parsed_args.export_dir,
+        )
+        deploy.deploy_components(components=parsed_args.components)
 
 
 def do_fetch():
     fetch.fetch_repositories(CONF.repositories.names)
 
 
-def do_cleanup():
-    cleanup.cleanup(auth_url=CONF.action.auth_url,
-                    skip_os_cleanup=CONF.action.skip_os_cleanup)
+class Fetch(BaseCommand):
+    """Fetch all repos with components definitions"""
 
-
-def do_show_dep():
-    if CONF.repositories.clone:
+    def take_action(self, parsed_args):
         do_fetch()
-    dependencies.show_dep(CONF.action.components)
+
+
+class Cleanup(BaseCommand):
+    """Remove all OpenStack resources and destroy CCP deployment"""
+
+    def get_parser(self, *args, **kwargs):
+        parser = super(Cleanup, self).get_parser(*args, **kwargs)
+        # Making auth url configurable at least until Ingress/LB support will
+        # be implemented
+        parser.add_argument('--auth-url',
+                            help='The URL of Keystone authentication '
+                                 'server')
+        parser.add_argument('--skip-os-cleanup',
+                            action='store_true',
+                            help='Skip cleanup of OpenStack environment')
+        return parser
+
+    def take_action(self, parsed_args):
+        cleanup.cleanup(auth_url=parsed_args.auth_url,
+                        skip_os_cleanup=parsed_args.skip_os_cleanup)
+
+
+class ShowDep(BaseCommand):
+    """Show dependencies of CCP components"""
+
+    def get_parser(self, *args, **kwargs):
+        parser = super(ShowDep, self).get_parser(*args, **kwargs)
+        parser.add_argument('components',
+                            nargs='+',
+                            help='CCP components to show dependencies')
+        return parser
+
+    def take_action(self, parsed_args):
+        if CONF.repositories.clone:
+            do_fetch()
+        dependencies.show_dep(parsed_args.components)
 
 
 def signal_handler(signo, frame):
     sys.exit(-signo)
 
 
-def main():
+class CCPApp(app.App):
+    CONSOLE_MESSAGE_FORMAT = \
+        '%(asctime)s %(levelname)-8s %(name)-15s %(message)s'
+
+    def __init__(self, **kwargs):
+        super(CCPApp, self).__init__(
+            description='Containerized Control Plane tool',
+            version=fuel_ccp.__version__,
+            command_manager=commandmanager.CommandManager('ccp.cli'),
+            **kwargs
+        )
+        self.config_file = None
+
+    @staticmethod
+    def add_config_file_argument(parser):
+        parser.add_argument(
+            '--config-file',
+            metavar='PATH',
+            help=('Path to a config file to use.'))
+
+    @staticmethod
+    def get_config_file(argv):
+        parser = argparse.ArgumentParser(add_help=False)
+        CCPApp.add_config_file_argument(parser)
+        parsed_args, _ = parser.parse_known_args(argv)
+        if parsed_args.config_file:
+            return os.path.abspath(os.path.expanduser(parsed_args.config_file))
+        else:
+            return config.find_config()
+
+    def run(self, argv):
+        self.config_file = self.get_config_file(argv)
+        config.setup_config(self.config_file)
+        self.add_config_file_argument(self.parser)
+        defaults = {k: CONF[k] for k in ['debug', 'verbose_level', 'log_file']}
+        self.parser.set_defaults(**defaults)
+        return super(CCPApp, self).run(argv)
+
+    def configure_logging(self):
+        super(CCPApp, self).configure_logging()
+        if self.config_file:
+            LOG.debug('Loaded config from file %s', self.config_file)
+        else:
+            LOG.debug('No config file loaded')
+
+
+def main(argv=None):
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
-    config.setup_config()
-
-    func = globals()['do_%s' % CONF.action.name.replace('-', '_')]
-    func()
+    if argv is None:
+        argv = sys.argv[1:]
+    CCPApp().run(argv)
 
 
 if __name__ == '__main__':
