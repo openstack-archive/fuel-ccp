8fb5753ad7a8e9c105a6f3624ecaf7faea1ccd13
Yuriy Taraday <yorik.sar@gmail.com>
2016-09-15 16:13:07 +0300

    Switch to cliff
    Added all commands and basic app for cliff, removed all
    legacy/compatibility stuff around CLI.
    
    Config file CLI argument and logging will be fixed in separate commit.
    
    Change-Id: Ib52b4c139c2ac9ea5afa073febcd764edced1464

--- fuel_ccp/tests/conf_fixture.py
+++ fuel_ccp/tests/conf_fixture.py
@@ -6,5 +6,5 @@ from fuel_ccp import config
 class Config(fixtures.Fixture):
     def _setUp(self):
         self.useFixture(fixtures.MockPatchObject(config, '_REAL_CONF'))
-        config.setup_config(['build'])
+        config.setup_config(None)
         self.conf = config._REAL_CONF
