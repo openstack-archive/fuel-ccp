cf34fb81249254cfcd06da3fb8362dd6c742ab08
Sergey Lukjanov <slukjanov@mirantis.com>
2016-09-16 16:15:02 -0700

    Add primitive support for specifying replicas
    It's a minimal support for specifying number of replicas for
    services in the ccp config file. It's badly needed to simplify
    testing on scale and testing of the running services in HA.
    It's not breaking any kind of backward compatibility, you're
    now able to specify it:
    
        replicas:
            keystone: 3
            nova-api: 42
    
    Corresponding verifications was added into the process of
    building topology as it's implicitly part of it.
    
    Additionally, there is a very small tests refactoring plus
    few new tests for replicas verification.
    
    Change-Id: Ia7e032fd3d2090cf42b05dfb96d0f0808d1399d6

--- fuel_ccp/deploy.py
+++ fuel_ccp/deploy.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import re
+import six
 
 from fuel_ccp.common import jinja_utils
 from fuel_ccp.common import utils
@@ -60,19 +61,21 @@ def _get_service_files_hash(service_dir, files, configs):
 
 def parse_role(service_dir, role, config):
     service = role["service"]
-    if service["name"] not in config.get("topology", {}):
+    service_name = service["name"]
+
+    if service_name not in config.get("topology", {}):
         LOG.info("Service %s not in topology config, skipping deploy",
-                 service["name"])
+                 service_name)
         return
-    LOG.info("Scheduling service %s deployment", service["name"])
+    LOG.info("Scheduling service %s deployment", service_name)
     _expand_files(service, role.get("files"))
 
     files_cm = _create_files_configmap(
-        service_dir, service["name"], role.get("files"))
+        service_dir, service_name, role.get("files"))
     meta_cm = _create_meta_configmap(service)
 
     workflows = _parse_workflows(service)
-    workflow_cm = _create_workflow(workflows, service["name"])
+    workflow_cm = _create_workflow(workflows, service_name)
     configmaps = config['configmaps'] + (files_cm, meta_cm, workflow_cm)
 
     cm_version = _get_configmaps_version(
@@ -89,16 +92,26 @@ def parse_role(service_dir, role, config):
     cont_spec = templates.serialize_daemon_pod_spec(service)
     affinity = templates.serialize_affinity(service, config["topology"])
 
+    replicas = config.get("replicas", {}).get(service_name)
     if service.get("daemonset", False):
-        obj = templates.serialize_daemonset(service["name"], cont_spec,
+        if replicas is not None:
+            LOG.error("Replicas was specified for %s, but it's implemented "
+                      "using Kubernetes DaemonSet that will deploy service on "
+                      "all matching nodes (section 'nodes' in config file)",
+                      service_name)
+            raise RuntimeError("Replicas couldn't be specified for services "
+                               "implemented using Kubernetes DaemonSet")
+
+        obj = templates.serialize_daemonset(service_name, cont_spec,
                                             affinity)
     else:
-        obj = templates.serialize_deployment(service["name"], cont_spec,
-                                             affinity)
+        replicas = replicas or 1
+        obj = templates.serialize_deployment(service_name, cont_spec,
+                                             affinity, replicas)
     kubernetes.process_object(obj)
 
     _create_service(service)
-    LOG.info("Service %s successfuly scheduled", service["name"])
+    LOG.info("Service %s successfuly scheduled", service_name)
 
 
 def _parse_workflows(service):
@@ -289,7 +302,7 @@ def _create_meta_configmap(service):
     return kubernetes.process_object(template)
 
 
-def _make_topology(nodes, roles):
+def _make_topology(nodes, roles, replicas):
     failed = False
     # TODO(sreshetniak): move it to validation
     if not nodes:
@@ -300,6 +313,9 @@ def _make_topology(nodes, roles):
         failed = True
     if failed:
         raise RuntimeError("Failed to create topology for services")
+
+    # Replicas are optional, 1 replica will deployed by default
+    replicas = replicas or dict()
 
     # TODO(sreshetniak): add validation
     k8s_nodes = kubernetes.list_k8s_nodes()
@@ -328,6 +344,26 @@ def _make_topology(nodes, roles):
                 service_to_node[svc].extend(roles_to_node[role])
         else:
             LOG.warning("Role '%s' defined, but unused", role)
+
+    replicas = replicas.copy()
+    for svc, svc_hosts in six.iteritems(service_to_node):
+        svc_replicas = replicas.pop(svc, None)
+
+        if svc_replicas is None:
+            continue
+
+        svc_hosts_count = len(svc_hosts)
+        if svc_replicas > svc_hosts_count:
+            LOG.error("Requested %s replicas for %s while only %s hosts able "
+                      "to run that service (%s)", svc_replicas, svc,
+                      svc_hosts_count, ", ".join(svc_hosts))
+            raise RuntimeError("Replicas doesn't match available hosts.")
+
+    if replicas:
+        LOG.error("Replicas defined for unspecified service(s): %s",
+                  ", ".join(replicas.keys()))
+        raise RuntimeError("Replicas defined for unspecified service(s)")
+
     return service_to_node
 
 
@@ -360,7 +396,8 @@ def deploy_components(components=None):
 
     config = utils.get_global_parameters("configs", "nodes", "roles")
     config["topology"] = _make_topology(config.get("nodes"),
-                                        config.get("roles"))
+                                        config.get("roles"),
+                                        config.get("replicas"))
 
     components_map = utils.get_deploy_components_info(config["configs"])
     components = set(components) if components else set(components_map.keys())
