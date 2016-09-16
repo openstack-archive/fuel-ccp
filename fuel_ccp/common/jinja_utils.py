90eebdf0faf78efb43809ef3320fe06e37422835
Kirill Bespalov <kbespalov@mirantis.com>
2016-09-16 13:38:25 +0300

    Introduce a generic config templating
    The base idea is allow to use generalized config
    templating via adding the cross repository loader.
    
    It may be helpful for configuring the values which copied
    from project to project like properties of oslo.messaging.
    
    Instead of dublicating the opts block in all projects, e.g:
    
    nova.conf.j2:
    ...
      [oslo_messaging_rabbit]
      rabbit_userid = {{ rabbitmq_user }}
      rabbit_password = {{ rabbitmq_password }}
      rabbit_ha_queues = true
      rabbit_hosts = rabbitmq
    ...
    
    we can replace it with a template located at single
    repository e.g. fuel-ccp-messaging
    
    nova.conf.j2:
    ...
      {% include "fuel-ccp-messaging/general.j2" %}
    ...
    
    and in it we can specify all messaging related options:
    
    general.j2:
    
      {% if rpc_backed == 'rabbit' %}
      [oslo_messaging_rabbit]
              ...
      {% endif %}
    
      {% if rpc_backed == 'zmq' %}
      [oslo_messaging_zmq]
            ...
      {% endif %}
            ...
    
    Change-Id: Ia474608f5eee109bec2b1d7d2af742340ea7e69f

--- fuel_ccp/common/jinja_utils.py
+++ fuel_ccp/common/jinja_utils.py
@@ -1,15 +1,35 @@
 import os
 
 import jinja2
+from fuel_ccp import config
 
 
 def str_to_bool(text):
     return text is not None and text.lower() in ['true', 'yes']
 
 
+def cross_repository_loader(relative_path):
+    """
+    Load a template source located at any fetched
+    repository by the relative path, e.g.:
+
+    fuel-ccp-messaging/generic_template.j2
+
+    :param relative_path: the path to template
+    """
+    repos_path = config.CONF.repositories.path
+    base_path = os.path.join(repos_path, os.path.dirname(relative_path))
+    filename = os.path.basename(relative_path)
+    loader = jinja2.FileSystemLoader(base_path)
+    return loader.get_source(None, filename)
+
+
 def jinja_render(path, context, functions=()):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        os.path.dirname(path)), undefined=jinja2.StrictUndefined)
+    env = jinja2.Environment(loader=jinja2.ChoiceLoader([
+        jinja2.FileSystemLoader(path),
+        jinja2.FunctionLoader(cross_repository_loader)]),
+        undefined=jinja2.StrictUndefined)
+
     env.filters['bool'] = str_to_bool
     for func in functions:
         env.globals[func.__name__] = func
