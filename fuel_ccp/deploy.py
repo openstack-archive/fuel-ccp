a512670de7dd2d7e352a06518dc2826a63677aa3
Kirill Bespalov <kbespalov@mirantis.com>
2016-09-30 02:37:49 +0300

    Fix dependencies resolution for services
    summary: Replace any service dependencies with set of its containers
    
    Take a look on the next case of DSL:
    
    service:
      name: A
      containers:
        - name: c1
        - name: c2
        - name: c3
    
    service:
      name: B
      containers:
        - name: fail
          daemon:
            dependencies:
              - A
    
    When service A will be started, the entrypoints scripts in containters
    of A will do reports to etcd that c1,c2 and c3 are done.
    
    So, the service B will wait indefinitely in this case, because the
    entrypoint script do report of state of container via its name, not
    service name (e.g. A).
    
    Change-Id: I6078bfbd863443aa545048b5f82ceaf8c671f857

--- fuel_ccp/deploy.py
+++ fuel_ccp/deploy.py
@@ -12,7 +12,6 @@ from fuel_ccp import kubernetes
 from fuel_ccp import templates
 from fuel_ccp.validation import deploy as deploy_validation
 
-
 CONF = config.CONF
 
 LOG = logging.getLogger(__name__)
@@ -392,6 +391,40 @@ def _create_openrc(config):
              os.getcwd(), config['namespace'])
 
 
+def _get_containers_names_by_service(service_name, components_map=None):
+    components_map = components_map or utils.get_deploy_components_info()
+    service_map = components_map[service_name]['service_content']['service']
+    names = [cntr['name'] for cntr in service_map['containers']]
+    return names
+
+
+def clarify_component_dependencies(component, components_map):
+    """Replace any service dependency with its containers."""
+
+    content_map = component['service_content']
+    containers = content_map['service']['containers']
+
+    for container in containers:
+        daemon_map = container['daemon']
+
+        # skip if there is no any dependency
+        if 'dependencies' not in daemon_map:
+            continue
+
+        deps = daemon_map['dependencies']
+        real_deps = set()
+        for dep in deps:
+            # check if the dep is a job
+            if dep not in components_map:
+                real_deps.add(dep)
+                continue
+            # otherwise just add the list of containers of a dependent service
+            names = _get_containers_names_by_service(dep, components_map)
+            real_deps = real_deps.union(names)
+
+        daemon_map['dependencies'] = list(real_deps)
+
+
 def deploy_components(components_map, components):
     if not components:
         components = set(components_map.keys())
@@ -414,6 +447,8 @@ def deploy_components(components_map, components):
     config['configmaps'] = (start_script_cm,)
 
     for component in components:
+        clarify_component_dependencies(components_map[component],
+                                       components_map)
         parse_role(components_map[component]['service_dir'],
                    components_map[component]['service_content'],
                    config)
