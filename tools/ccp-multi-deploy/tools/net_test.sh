343feb9fc2697054eed0f3d3887317da83f5f3f1
Sergey Lukjanov <slukjanov@mirantis.com>
2016-09-30 07:46:11 -0700

    Initial commit for ccp multi-deploy solution
    It's authored by Marek, I'm just reposting it here to make
    publically available.
    This is a sample shell script + config files to assist in deployment
    of multiple OpenStack environments within a single K8s cluster.
    
    Change-Id: Ic372d56b72b2430cabdb8afb84d2d7776a349465

---
+++ tools/ccp-multi-deploy/tools/net_test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -ex
+#
+# Basic networking check for K8s, please adjust to your needs.
+# Originally adopted from openstack/fuel-ccp-installer
+# by kproskurin@mirantis.com
+
+SLAVE_IPS=(node1 node2 node3 node4 node5 node6 node7 node8 node9)
+ADMIN_IP="node1"
+if [[ "$SLAVE_IPS" == "changeme" || "$ADMIN_IP" == "changeme" ]];then
+    echo "Please set variables SLAVE_IPS and ADMIN_IP."
+    exit 1
+fi
+ADMIN_USER="vagrant"
+SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownhostsFile=/dev/null"
+kubedns_ip=$(sudo -u vagrant -s ssh $SSH_OPTIONS $ADMIN_USER@$ADMIN_IP kubectl get svc --namespace kube-system kubedns --template={{.spec.clusterIP}})
+dnsmasq_ip=$(sudo -u vagrant -s ssh $SSH_OPTIONS $ADMIN_USER@$ADMIN_IP kubectl get svc --namespace kube-system dnsmasq --template={{.spec.clusterIP}})
+domain="cluster.local"
+internal_test_domain="kubernetes"
+external_test_domain="kubernetes.io"
+declare -A node_ip_works
+declare -A node_internal_dns_works
+declare -A node_external_dns_works
+declare -A container_dns_works
+declare -A container_hostnet_dns_works
+failures=0
+for node in "${SLAVE_IPS[@]}"; do
+    # Check UDP 53 for kubedns
+   if sudo -u vagrant -s ssh  $SSH_OPTIONS $ADMIN_USER@$node nc -uzv $kubedns_ip 53 >/dev/null; then
+        node_ip_works["${node}"]="PASSED"
+    else
+        node_ip_works["${node}"]="FAILED"
+        (( failures++ ))
+    fi
+    # Check internal lookup
+   if sudo -u vagrant -s ssh  $SSH_OPTIONS $ADMIN_USER@$node nslookup $internal_test_domain $kubedns_ip >/dev/null; then
+        node_internal_dns_works["${node}"]="PASSED"
+    else
+        node_internal_dns_works["${node}"]="FAILED"
+        (( failures++ ))
+    fi
+    # Check external lookup
+   if sudo -u vagrant -s ssh  $SSH_OPTIONS $ADMIN_USER@$node nslookup $external_test_domain $dnsmasq_ip >/dev/null; then
+        node_external_dns_works[$node]="PASSED"
+    else
+        node_external_dns_works[$node]="FAILED"
+        (( failures++ ))
+    fi
+    # Check UDP 53 for kubedns in container
+   if sudo -u vagrant -s ssh  $SSH_OPTIONS $ADMIN_USER@$node sudo docker run --rm busybox nslookup $external_test_domain $dnsmasq_ip >/dev/null; then
+        container_dns_works[$node]="PASSED"
+    else
+        container_dns_works[$node]="FAILED"
+        (( failures++ ))
+    fi
+    # Check UDP 53 for kubedns in container with host networking
+   if sudo -u vagrant -s ssh  $SSH_OPTIONS $ADMIN_USER@$node sudo docker run --net=host --rm busybox nslookup $external_test_domain $dnsmasq_ip >/dev/null; then
+        container_hostnet_dns_works[$node]="PASSED"
+    else
+        container_hostnet_dns_works[$node]="FAILED"
+        (( failures++ ))
+    fi
+done
+# Report results
+echo "Found $failures failures."
+for node in "${SLAVE_IPS[@]}"; do
+    echo
+    echo "Node $node status:"
+    echo "  Node to container communication: ${node_ip_works[$node]}"
+    echo "  Node internal DNS lookup (via kubedns): ${node_internal_dns_works[$node]}"
+    echo "  Node external DNS lookup (via dnsmasq): ${node_external_dns_works[$node]}"
+    echo "  Container internal DNS lookup (via kubedns): ${container_dns_works[$node]}"
+    echo "  Container internal DNS lookup (via kubedns): ${container_hostnet_dns_works[$node]}"
+done
