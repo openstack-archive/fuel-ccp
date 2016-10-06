8b4d7522946623a51e53e081dd8d842d33be4689
Marek Zawadzki <mzawadzki@mirantis.com>
2016-10-06 10:34:18 +0200

    Add new script into tools for K8s deployment
    Sample script to deploy K8s cluster with Kargo.
    It is meant to be adjusted to individual needs.
    
    Change-Id: If3ce2cb12d5c5363fca2554b30dad3e05bc9fcf9

---
+++ tools/deploy_k8s_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+#
+# Sample script to deploy K8s cluster with Kargo.
+# Please adjust to your needs.
+
+
+# Configuration:
+export ENV_NAME="kargo-test-9-nodes-new"
+export IMAGE_PATH="/home/ubuntu/packer-ubuntu-1604-server.qcow2" # path to VM image file (e.g. build with packer)
+export DONT_DESTROY_ON_SUCCESS=1
+#export VLAN_BRIDGE="vlan456" # full name e.g. "vlan450"
+export DEPLOY_METHOD="kargo"
+export SLAVES_COUNT=3
+export SLAVE_NODE_MEMORY=6144
+export SLAVE_NODE_CPU=2
+export WORKSPACE="/home/ubuntu/workspace"
+CCP_INSTALLER_DIR="/home/ubuntu/fuel-ccp-installer"
+export CUSTOM_YAML='hyperkube_image_repo: "quay.io/coreos/hyperkube"
+hyperkube_image_tag: "v1.4.0_coreos.1"
+kube_version: "v1.4.0"'
+
+
+mkdir -p ${WORKSPACE}
+echo "Running on ${NODE_NAME}: ${ENV_NAME}"
+pushd ${CCP_INSTALLER_DIR}
+bash -x "./utils/jenkins/run_k8s_deploy_test.sh"
+popd
