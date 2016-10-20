c2056474315d4ad8ad4c2f58e33d626a878a2987
Yuriy Taraday <yorik.sar@gmail.com>
2016-10-20 17:12:39 +0300

    Add dockerfiles validation
    The validation takes 2 steps:
    a) render Dockerfile.j2's;
    b) verify that all images have parents except ones that are listed in
       base_images config parameter.
    
    This validation ignores list of components because we have repos without
    services (debian-base, openstack-base).
    
    Change-Id: I0fd9fb0382b860f04e12993e350c7c23fbcdfda7

--- fuel_ccp/validate.py
+++ fuel_ccp/validate.py
@@ -1,17 +1,21 @@
 from fuel_ccp.common import utils
 
+from fuel_ccp.validation import dockerfiles
 from fuel_ccp.validation import service as service_validation
 
 
 def validate(components, types):
     if not types:
-        types = ["service-def"]
+        types = ["service-def", "dockerfiles"]
 
     for validation_type in set(types):
         if validation_type == "service-def":
             component_map = utils.get_deploy_components_info()
             service_validation.validate_service_definitions(component_map,
                                                             components)
+        elif validation_type == "dockerfiles":
+            component_map = utils.get_deploy_components_info()
+            dockerfiles.validate(component_map, components)
         else:
             raise RuntimeError(
                 "Unexpected validation type: '{}'".format(validation_type)
