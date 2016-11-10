d1c85895294c46c9f39e69769548f36c94527d54
Andrey Pavlov <apavlov@mirantis.com>
2016-11-10 19:17:11 +0300

    Adding 'ccp domains list' command
    This command will print list of Ingress domains
    that need to be resolved in order to make Ingress work
    
    Change-Id: Ie7599d2833fb34facdc66f008fe615a03abd2f33

--- fuel_ccp/cli.py
+++ fuel_ccp/cli.py
@@ -225,6 +225,22 @@ class ImagesList(BaseCommand, lister.Lister):
         )
 
 
+class DomainsList(BaseCommand, lister.Lister):
+    """Get Ingress domains that will be used for external access"""
+
+    def get_parser(self, *args, **kwargs):
+        parser = super(DomainsList, self).get_parser(*args, **kwargs)
+        parser.add_argument('components',
+                            nargs='*',
+                            help='CCP components to get domains for')
+        return parser
+
+    def take_action(self, parsed_args):
+        config.load_component_defaults()
+        domains_list = utils.get_ingress_domains(parsed_args.components)
+        return ('Ingress Domain',), zip(domains_list)
+
+
 def signal_handler(signo, frame):
     sys.exit(-signo)
 
