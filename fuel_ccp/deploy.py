06c4f5a4e09b2a538da6be9348243ff89d1176d1
Kirill Bespalov <kbespalov@mirantis.com>
2016-11-10 20:04:00 +0300

    Introduce cross-repository config templating
    Make possible sharing and using of common parts of configs
    like keystone, db, messaging, etc as jinja templates (e.g.
    via macros) located at 'exports' directories of related
    repositories.
    
    Example of usage:
    -------------------------------------------------
    share rabbitmq configuration as macros:
    -------------------------------------------------
      # file fuel-ccp-rabbitmq/exports/messaging.j2
      {% macro oslo_config() -%}
        [DEFAULT]
        transport_url=rabbit://{{ rabbitmq.user }}
        [oslo_messaging_rabbit]
        rabbit_ha_queues = true
      {%- endmacro %}
    -------------------------------------------------
    use it in nova.conf.j2:
    -------------------------------------------------
      # file fuel-ccp-nova/service/files/nova.conf.j2
      [upgrade_levels]
      compute = auto
    
      {{ messaging.oslo_config() }} <-----------
    
      [wsgi]
      api_paste_config = /etc/nova/api-paste.ini
     -------------------------------------------------
    
    During 'ccp deploy' the following occurs:
    
    - loading templates files from /exports/ dirs of avaliable repositories.
    - push files to k8s as ConfigMap with name 'exports'.
    - adding a container volume '/etc/ccp/macros' with the ConfigMap content
    - implicitly adding jinja imports of these templates files to all config
      files from /fuel-ccp-xxx/service/* to make possible macros usage.
    
    Change-Id: I4858d62a9713e90c09300f75e01e06a31d3ac0ae
    Depends-On: I429656b7eaf6312ee2d27ccaf0cb8802a234e871

--- fuel_ccp/deploy.py
+++ fuel_ccp/deploy.py
@@ -61,7 +61,7 @@ def _get_service_files_hash(service_dir, files, configs):
     return hashlib.sha1(dump).hexdigest()
 
 
-def parse_role(component, topology, configmaps):
+def parse_role(component, topology, configmaps, jinja_imports):
     service_dir = component["service_dir"]
     role = component["service_content"]
     component_name = component["component_name"]
@@ -72,7 +72,7 @@ def parse_role(component, topology, configmaps):
     _expand_files(service, role.get("files"))
 
     files_cm = _create_files_configmap(
-        service_dir, service_name, role.get("files"))
+        service_dir, service_name, role.get("files"), jinja_imports)
     meta_cm = _create_meta_configmap(service)
 
     workflows = _parse_workflows(service)
@@ -295,14 +295,15 @@ def _create_start_script_configmap():
     return kubernetes.process_object(cm)
 
 
-def _create_files_configmap(service_dir, service_name, files):
+def _create_files_configmap(service_dir, service_name, files, macros_imports):
     configmap_name = "%s-%s" % (service_name, templates.FILES_CONFIG)
     data = {}
     if files:
-        for filename, f in files.items():
-            with open(os.path.join(
-                    service_dir, "files", f["content"]), "r") as f:
-                data[filename] = f.read()
+        for filename, desc in files.items():
+            path = os.path.join(service_dir, "files", desc["content"])
+            with open(path) as f:
+                # Implicitly add macros imports to the beginning of the file.
+                data[filename] = '\n'.join([macros_imports, f.read()])
     data["placeholder"] = ""
     template = templates.serialize_configmap(configmap_name, data)
     return kubernetes.process_object(template)
@@ -317,6 +318,13 @@ def _create_meta_configmap(service):
     }
     template = templates.serialize_configmap(configmap_name, data)
     return kubernetes.process_object(template)
+
+
+def _create_jinja_templates_configmap(templates_files):
+    """Create config map of files from fuel-ccp-repo/exports dirs."""
+    serialized = templates.serialize_configmap(templates.EXPORTS_CONFIG,
+                                               templates_files)
+    return kubernetes.process_object(serialized)
 
 
 def _make_topology(nodes, roles, replicas):
@@ -525,14 +533,17 @@ def deploy_components(components_map, components):
     _create_namespace(CONF.configs)
     _create_globals_configmap(CONF.configs)
     start_script_cm = _create_start_script_configmap()
-    configmaps = (start_script_cm,)
+
+    # Create cm with jinja config templates shared across all repositories.
+    templates_files = utils.get_repositories_exports()
+    jinja_imports = jinja_utils.generate_jinja_imports(templates_files.keys())
+    templates_cm = _create_jinja_templates_configmap(jinja_imports)
+    configmaps = (start_script_cm, templates_cm)
 
     upgrading_components = {}
     for service_name in components:
         service = components_map[service_name]
-        objects_gen = parse_role(service,
-                                 topology=topology,
-                                 configmaps=configmaps)
+        objects_gen = parse_role(service, topology, configmaps, jinja_imports)
         objects = list(itertools.chain.from_iterable(objects_gen))
         component_name = service['component_name']
         do_upgrade = component_name in upgrading_components
