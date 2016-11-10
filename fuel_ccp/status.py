e51f254eda7198abf0b9015c256bb7e6c0c87176
Sergey Reshetnyak <sreshetniak@mirantis.com>
2016-11-10 16:31:12 +0300

    Optimize 'ccp status' command
    Now it works for 3 requests instead deployment_count + daemonset_count +
    3 requests
    
    Change-Id: Icb941c183ed05d3686667b7fa1644bebea684077

--- fuel_ccp/status.py
+++ fuel_ccp/status.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import copy
 import logging
 
 from fuel_ccp import config
@@ -9,78 +10,54 @@ CONF = config.CONF
 
 LOG = logging.getLogger(__name__)
 
-
-class State(object):
-    def __init__(self, name, total, running, urls):
-        self.name = name
-        self.total = total or 0
-        self.running = running or 0
-        self.job_completed = 0
-        self.job_total = 0
-        self.urls = urls or []
-
-    def __repr__(self):
-        if self.ready:
-            return "ok"
-        else:
-            return "nok"  # not ready
-
-    def __lt__(self, other):
-        return self.name.__lt__(other.name)
-
-    def __bool__(self):
-        return self.ready
-
-    def __nonzero__(self):
-        return self.ready
-
-    @property
-    def ready(self):
-        return (self.total == self.running
-                and self.job_total == self.job_completed)
+EXT_LINK_TEMPLATE = "http://{ext_ip}:{port}"
+STATE_TEMPLATE = {
+    "pod_total": 0,
+    "pod_running": 0,
+    "job_total": 0,
+    "job_completed": 0,
+    "links": []
+}
 
 
-def _get_pods_status(service, svc_map):
-    pods = kubernetes.list_cluster_pods(service=service)
-    total = running = 0
-    for pod in pods:
-        total += 1
-        if pod.ready:
-            running += 1
-    return State(
-        name=service,
-        total=total,
-        running=running,
-        urls=svc_map.get(service, []))
+def is_app_ready(state):
+    return (state["pod_total"] == state["pod_running"]
+            and state["job_total"] == state["job_completed"])
+
+
+def repr_state(state):
+    return "ok" if is_app_ready(state) else "nok"
 
 
 def get_pod_states(components=None):
     ext_ip = CONF.configs.get("k8s_external_ip", "")
-    ext_link_template = "http://{ext_ip}:{port}"
-    states = []
-    svc_map = {}
+
+    states = {}
+    for pod in kubernetes.list_cluster_pods():
+        app_name = pod.obj["metadata"]["labels"].get("app")
+        if not app_name:
+            continue
+        states.setdefault(app_name, copy.deepcopy(STATE_TEMPLATE))
+        states[app_name]["pod_total"] += 1
+        if pod.ready:
+            states[app_name]["pod_running"] += 1
+
+    for job in kubernetes.list_cluster_jobs():
+        app_name = job.obj["metadata"]["labels"].get("app")
+        if not app_name:
+            continue
+        states.setdefault(app_name, copy.deepcopy(STATE_TEMPLATE))
+        states[app_name]["job_total"] += job.obj["spec"]["completions"]
+        states[app_name]["job_completed"] += (
+            job.obj["status"].get("succeeded", 0))
+
     for svc in kubernetes.list_cluster_services():
         svc_name = svc.obj["metadata"]["name"]
-        svc_map.setdefault(svc_name, [])
+        states.setdefault(svc_name, copy.deepcopy(STATE_TEMPLATE))
         for port in svc.obj["spec"]["ports"]:
-            svc_map[svc_name].append(ext_link_template.format(
+            states[svc_name]["links"].append(EXT_LINK_TEMPLATE.format(
                 ext_ip=ext_ip,
                 port=port["nodePort"]))
-    for dp in kubernetes.list_cluster_deployments():
-        if not components or dp.name in components:
-            states.append(_get_pods_status(dp.name, svc_map))
-
-    job_states = {}
-    for job in kubernetes.list_cluster_jobs():
-        service = job.obj["metadata"]["labels"].get("app")
-        job_states.setdefault(service, {"completed": 0, "total": 0})
-        job_states[service]["total"] += job.obj["spec"]["completions"]
-        job_states[service]["completed"] += job.obj["status"].get("succeeded",
-                                                                  0)
-    for state in states:
-        if state.name in job_states:
-            state.job_total = job_states[state.name]["total"]
-            state.job_completed = job_states[state.name]["completed"]
 
     return states
 
@@ -92,16 +69,21 @@ def show_long_status(components=None):
     formatted_states = []
 
     for state in sorted(states):
-        formatted_states.append((
-            state.name,
-            "%d/%d" % (state.running, state.total),
-            "%d/%d" % (state.job_completed, state.job_total),
-            state,
-            "\n".join(state.urls)))
+        if not components or state in components:
+            formatted_states.append((
+                state,
+                "{pod_running}/{pod_total}".format(**states[state]),
+                "{job_completed}/{job_total}".format(**states[state]),
+                repr_state(states[state]),
+                "\n".join(states[state]["links"])))
 
     return columns, formatted_states
 
 
 def show_short_status():
-    status = "ok" if all(get_pod_states()) else "nok"
+    states = get_pod_states()
+    if not states:
+        status = "no cluster"
+    else:
+        status = "ok" if all(map(is_app_ready, states.values())) else "nok"
     return ("status",), ((status,),)
