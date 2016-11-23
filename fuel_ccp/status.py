09fb050815863205382a238623ea915513be0356
Sergey Reshetnyak <sreshetniak@mirantis.com>
2016-11-23 18:01:28 +0300

    Fix getting replicas count in ccp status
    replicas key is not available in status info during creating and
    upgrading deployment
    
    Change-Id: I6dd02630a8e7d7fa39e9676ba0e53456e25b9ce5

--- fuel_ccp/status.py
+++ fuel_ccp/status.py
@@ -52,7 +52,7 @@ def get_pod_states(components=None):
     for dp in kubernetes.list_cluster_deployments():
         states.setdefault(dp.name, copy.deepcopy(STATE_TEMPLATE))
         dp_st = dp.obj["status"]
-        states[dp.name]["pod_total"] = dp_st["replicas"]
+        states[dp.name]["pod_total"] = dp.obj["spec"]["replicas"]
         states[dp.name]["pod_running"] = min(
             dp_st.get("availableReplicas", 0), dp_st["updatedReplicas"])
 
