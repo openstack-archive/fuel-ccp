be08f9308d98c29d018871e632ed8ef4bf9587fe
Dmitry Klenov <dklenov@mirantis.com>
2016-11-29 09:18:03 +0000

    Containerizing fuel ccp
    * Ability to create docker container out of current state
    of the repository has been added.
    * Alias to execute 'ccp' command transparently from the host system
    is generated.
    * Ability to create container out of remote repository.
    
    Change-Id: Iafcad1190a63dbc2c10b129f50bf47d70d2faa65

---
+++ docker/ccp/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Adds a local user
+# Either use the LOCAL_USER_ID and LOCAL_USER_NAME if passed in at runtime
+# or user with id 9001 and name ccp as a fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+USER_NAME=${LOCAL_USER_NAME:-ccp}
+
+groupadd docker -g `stat -c " %g" /var/run/docker.sock`
+useradd --shell /bin/bash -u $USER_ID -G docker -o -m $USER_NAME 2> /dev/null
+export HOME=/home/$USER_NAME
+chown $USER_NAME:$USER_NAME /home/$USER_NAME/
+
+exec /usr/local/bin/gosu $USER_NAME ccp "$@"
