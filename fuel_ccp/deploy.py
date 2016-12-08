d16f1e6b4bbbf0f2916f2317c6043de9f4312e75
Krzysztof Szukiełojć <kszukielojc@mirantis.com>
2016-12-08 08:24:26 +0100

    Adding Support for Annotations in DSL
    Full Annotations support for pod and service is needed for setting
    extra options in kubernates for users.
    
    Change-Id: Icbde776e5e8b44cfabe752fb43cab2ed9978ffe5

--- fuel_ccp/deploy.py
+++ fuel_ccp/deploy.py
@@ -125,7 +125,14 @@ def parse_role(component, topology, configmaps, jinja_imports):
     else:
         replicas = replicas or 1
 
-    obj = templates.serialize_deployment(service_name, cont_spec, affinity,
+    annotations = service.get('annotations', {}).get('service')
+    same_keywords = set(annotations) & set(affinity)
+    if same_keywords:
+        raise RuntimeError(
+            'Affinity is in conflict with annotations with key: %s'
+            .format(same_keywords))
+    annotations.update(affinity)
+    obj = templates.serialize_deployment(service_name, cont_spec, annotations,
                                          replicas, component_name, strategy)
     yield [obj]
 
@@ -192,7 +199,9 @@ def _process_ports(service):
             if ingress_host:
                 ingress_rules.append(templates.serialize_ingress_rule(
                     service["name"], ingress_host, source_port))
-    service_template = templates.serialize_service(service["name"], ports)
+    service_template = templates.serialize_service(
+        service["name"], ports,
+        annotations=service.get('annotations', {}).get('service'))
     yield service_template
 
     if ingress_rules:
