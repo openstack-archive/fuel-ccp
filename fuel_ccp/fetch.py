deb512576bdd80e6554c73ce5e30a6f4a5290872
Anastasiya <atolochkova@mirantis.com>
2017-01-20 08:58:12 +0400

    Implement error handling by ccp fetch
    Change-Id: I315893243a7682243b81ac379615d4d18338fefd

--- fuel_ccp/fetch.py
+++ fuel_ccp/fetch.py
@@ -13,21 +13,62 @@ LOG = logging.getLogger(__name__)
 
 FETCH_TIMEOUT = 2 ** 16  # in seconds
 
+STATUSES = {
+    'fetch_alredy_existed': 'AlreadyExist',
+    'fetch_failed': 'Failure',
+    'fetch_succeeded': 'Success'
+}
+
 
 def fetch_repository(repository_def):
     name = repository_def['name']
     dest_dir = os.path.join(CONF.repositories.path, name)
+    result = {'name': name, 'status': STATUSES['fetch_failed']}
     if os.path.isdir(dest_dir):
         LOG.debug('%s: Repository is already cloned, skipping', name)
-        return
+        result.update(status=STATUSES['fetch_alredy_existed'])
+        return result
     git_url = repository_def['git_url']
     git_ref = repository_def.get('git_ref')
     LOG.debug('%s: Cloning repository %s to %s', name, git_url, dest_dir)
-    repo = git.Repo.clone_from(git_url, dest_dir)
-    if git_ref:
+    try:
+        repo = git.Repo.clone_from(git_url, dest_dir)
+    except git.exc.InvalidGitRepositoryError:
+        LOG.error('%s: Repository does not exist', name)
+        return result
+    if git_ref and repo:
         LOG.debug('%s: Changing reference to "%s"', name, git_ref)
-        repo.git.checkout(git_ref)
+        try:
+            repo.git.checkout(git_ref)
+        except git.exc.CheckoutError:
+            LOG.error('%s: Failed to checkout %s', name, git_ref)
+            return result
     LOG.info('%s: Repository has been cloned', name)
+    result.update(status=STATUSES['fetch_succeeded'])
+    return result
+
+
+def _get_summary(fetch_info):
+    LOG.info('#' * 50)
+    LOG.info('Summary:')
+    fetch_succeeded = [info['name'] for info in fetch_info
+                       if info['status'] == 'Success']
+    fetch_failed = [info['name'] for info in fetch_info
+                    if info['status'] == 'Failure']
+    fetch_alredy_existed = [info['name'] for info in fetch_info
+                            if info['status'] == 'AlreadyExist']
+    if fetch_succeeded:
+        LOG.info('%d repository(s) cloning succeeded: %s' % (
+            len(fetch_succeeded), ', '.join(fetch_succeeded)))
+    if fetch_alredy_existed:
+        LOG.info('%d repository(s) is(are) already cloned: %s' % (
+            len(fetch_alredy_existed), ', '.join(fetch_alredy_existed)))
+    if fetch_failed:
+        LOG.info('%d repository(s) cloning failed: %s' % (
+            len(fetch_failed), ', '.join(fetch_failed)))
+    LOG.info('#' * 50)
+    if fetch_failed:
+        raise Exception("Failed to fetch %d repos" % len(fetch_failed))
 
 
 def fetch_repositories(repository_defs=None):
@@ -36,7 +77,6 @@ def fetch_repositories(repository_defs=None):
 
     LOG.info('Cloning repositories into %s', CONF.repositories.path)
 
-    errors = 0
     with futures.ThreadPoolExecutor(
             max_workers=CONF.repositories.clone_concurrency) as executor:
         future_list = []
@@ -46,18 +86,14 @@ def fetch_repositories(repository_defs=None):
                     fetch_repository, repository_def
                 ))
 
+            fetch_statuses = []
             for future in future_list:
-                try:
-                    # we need to use timeout because in this case python
-                    # thread wakes up time to time to check timeout and don't
-                    # block signal processing
-                    future.result(timeout=FETCH_TIMEOUT)
-                except Exception as ex:
-                    LOG.error("Failed to fetch: %s" % ex)
-                    errors += 1
+                # we need to use timeout because in this case python
+                #  thread wakes up time to time to check timeout and don't
+                #  block signal processing
+                fetch_statuses.append(future.result(timeout=FETCH_TIMEOUT))
         except SystemExit:
             for future in future_list:
                 future.cancel()
             raise
-    if errors:
-        raise Exception("Failed to fetch %d repos" % errors)
+    _get_summary(fetch_statuses)
--- fuel_ccp/tests/test_fetch.py
+++ fuel_ccp/tests/test_fetch.py
@@ -1,3 +1,5 @@
+import git
+import mock
 import os
 
 import fixtures
@@ -6,14 +8,29 @@ from fuel_ccp.tests import base
 import testscenarios
 
 
+STATUSES = {
+    'fetch_alredy_existed': 'AlreadyExist',
+    'fetch_failed': 'Failure',
+    'fetch_succeeded': 'Success'
+}
+
+
 class TestFetch(testscenarios.WithScenarios, base.TestCase):
     component_def = {'name': 'compname', 'git_url': 'theurl'}
     update_def = {}
-    expected_clone_call = None
     dir_exists = False
+    clone_side_effect = None
+    checkout_side_effect = None
 
     scenarios = [
         ('exists', {'dir_exists': True}),
+        ('clone_exception', {
+            'clone_side_effect': git.exc.InvalidGitRepositoryError()}),
+        ('clone', {}),
+        ('checkout', {'update_def': {'git_ref': 'theref'}}),
+        ('checkout_exception', {'update_def': {'git_ref': 'theref'},
+                                'checkout_side_effect':
+                                    git.exc.CheckoutError('msg', [], [], [])})
     ]
 
     def setUp(self):
@@ -21,8 +38,16 @@ class TestFetch(testscenarios.WithScenarios, base.TestCase):
         # Creating temporaty directory for repos
         self.tmp_path = self.useFixture(fixtures.TempDir()).path
         self.conf['repositories']['path'] = self.tmp_path
-        fixture = fixtures.MockPatch('git.Repo.clone_from')
-        self.mock_clone = self.useFixture(fixture).mock
+        if self.clone_side_effect:
+            fixture_clone = fixtures.MockPatch('git.Repo.clone_from')
+            self.mock_clone = self.useFixture(fixture_clone).mock
+            self.mock_clone.side_effect = self.clone_side_effect
+        else:
+            repo = mock.Mock()
+            repo.git.checkout.side_effect = self.checkout_side_effect
+            fixture_clone = fixtures.MockPatch('git.Repo.clone_from',
+                                               return_value=repo)
+            self.mock_clone = self.useFixture(fixture_clone).mock
 
     def test_fetch_repository(self):
         component_def = self.component_def.copy()
@@ -32,16 +57,29 @@ class TestFetch(testscenarios.WithScenarios, base.TestCase):
         isdir_mock = self.useFixture(fixture).mock
         isdir_mock.return_value = self.dir_exists
 
-        fetch.fetch_repository(component_def)
-
+        status = fetch.fetch_repository(component_def)
         git_path = os.path.join(self.tmp_path, component_def['name'])
         isdir_mock.assert_called_once_with(git_path)
-        if self.expected_clone_call:
+        if self.dir_exists:
+            self.assertEqual(status['status'],
+                             STATUSES['fetch_alredy_existed'])
+            self.mock_clone.assert_not_called()
+
+        elif self.clone_side_effect:
+            self.mock_clone.assert_called_once_with('theurl', git_path)
+            self.assertEqual(status['status'],
+                             STATUSES['fetch_failed'])
+        elif not self.clone_side_effect:
             git_ref = component_def.get('git_ref')
-            if git_ref:
+            if git_ref and self.checkout_side_effect:
+                self.assertEqual(status['status'],
+                                 STATUSES['fetch_failed'])
+            elif git_ref:
                 self.mock_clone.assert_called_once_with(
-                    'theurl', git_path, branch=git_ref)
+                    'theurl', git_path)
+                self.assertEqual(status['status'],
+                                 STATUSES['fetch_succeeded'])
             else:
                 self.mock_clone.assert_called_once_with('theurl', git_path)
-        else:
-            self.mock_clone.assert_not_called()
+                self.assertEqual(status['status'],
+                                 STATUSES['fetch_succeeded'])
