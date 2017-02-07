35b7e1f5a72a74c7965fd78260014b2f7347e9d0
Peter Razumovsky <prazumovsky@mirantis.com>
2017-02-07 14:48:30 +0400

    Add validation for nodes configs section
    Add check for nodes configs section - it should be a dict.
    
    Change-Id: Ie0114881aca4f62cfd50ff5358808765aa2924f8

--- fuel_ccp/validation/deploy.py
+++ fuel_ccp/validation/deploy.py
@@ -1,5 +1,10 @@
+import logging
+
 from fuel_ccp.common import utils
+from fuel_ccp.config import _yaml
 from fuel_ccp import dependencies
+
+LOG = logging.getLogger(__name__)
 
 
 def validate_requested_components(components, components_map):
@@ -17,3 +22,36 @@ def validate_requested_components(components, components_map):
         raise RuntimeError('Following components are also required for '
                            'successful deployment: '
                            '%s' % ' '.join(not_provided_components))
+
+
+def validate_nodes_section(nodes, configs):
+    valid = True
+    if not nodes:
+        LOG.error("Nodes section is not specified in configs")
+        valid = False
+    else:
+        for name in nodes:
+            if 'configs' in nodes[name]:
+                if not isinstance(nodes[name]['configs'], _yaml.AttrDict):
+                    LOG.error("Nodes configs should be a dict, found "
+                              "%s" % type(nodes[name]['configs']))
+                    valid = False
+                    break
+                else:
+                    valid = validate_nodes_config(nodes[name]['configs'],
+                                                  configs)
+                    if not valid:
+                        break
+    return valid
+
+
+def validate_nodes_config(node_config, global_config):
+    for k in node_config:
+        if k not in global_config:
+            LOG.error('Nodes configs cannot contain new variables, just '
+                      'override existent')
+            return False
+        elif (isinstance(node_config[k], (dict, _yaml.AttrDict)) and
+              isinstance(global_config[k], (dict, _yaml.AttrDict))):
+            return validate_nodes_config(node_config[k], global_config[k])
+    return True
