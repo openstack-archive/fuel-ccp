22d0c44b2992cd5572784b909748bd0ad4b4bedf
Andrey Pavlov <apavlov@mirantis.com>
2017-02-17 16:14:19 +0400

    Configs overrides for custom services
    "configs" section added to "services" group.
    
    services:
      some_service:
        service_def: ads
        configs:
          asd: dsa
    
    If you are mapping some service to some another service,
    configs will be inherited as well.
    
    Change-Id: Id64c0bf816a639c0b3dee96e5a72fcf964f9f731

--- fuel_ccp/tests/common/test_utils.py
+++ fuel_ccp/tests/common/test_utils.py
@@ -49,45 +49,6 @@ class TestUtils(base.TestCase):
 
         self.assertDictEqual(expected, res)
 
-    def test_get_deploy_components_info_with_custom_context(self):
-
-        custom_params = {
-            "configs": {
-                "service_name": "keystone",
-                "db_root_password": "db_root_password_custom",
-                "keystone_db_name": "keystone_db_name_custom",
-                "keystone_db_username": "keystone_db_username_custom",
-                "keystone_db_password": "keystone_db_password_custom",
-                "openstack_user_password": "os_user_password_custom",
-                "openstack_user_name": "os_user_name_custom",
-                "openstack_project_name": "os_project_name_custom",
-                "openstack_role_name": "os_role_name_custom",
-                "keystone_admin_port": "keystone_admin_port_custom",
-                "keystone_public_port": "keystone_public_port_custom"
-            }
-        }
-
-        conf = config._yaml.AttrDict()
-        conf._merge(custom_params)
-        conf._merge(config._REAL_CONF)
-        config._REAL_CONF = conf
-        base_dir = os.path.dirname(__file__)
-
-        self.conf.repositories.path = os.path.join(base_dir, "test_repo_dir")
-        self.conf.repositories.repos = [{"name": "component"}]
-
-        config.load_component_defaults()
-
-        res = utils.get_deploy_components_info(
-            rendering_context=custom_params["configs"]
-        )["keystone"]["service_content"]
-
-        with open(os.path.join(base_dir,
-                               "service-rendered-example-custom.yaml")) as f:
-            expected = yaml.load(f)
-
-        self.assertDictEqual(expected, res)
-
     def test_get_deploy_components_info_with_not_enough_context(self):
 
         default_params = {
