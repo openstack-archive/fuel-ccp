77466680d229ccfe1eacbaab284366c7fd6d57db
Andrey Pavlov <apavlov@mirantis.com>
2017-02-20 22:12:53 +0400

    Initial AppController integration
    Change-Id: Ic3dc48a93f2cc1a4d3ff3e513b5cfcc545289c6b

--- fuel_ccp/templates.py
+++ fuel_ccp/templates.py
@@ -357,7 +357,7 @@ def serialize_volumes(service, for_job=None):
 
 
 def serialize_job(name, spec, component_name, app_name):
-    return {
+    job = {
         "apiVersion": "batch/v1",
         "kind": "Job",
         "metadata": {
@@ -372,6 +372,16 @@ def serialize_job(name, spec, component_name, app_name):
             "template": spec
         }
     }
+    if CONF.kubernetes.appcontroller.enabled:
+        job = {
+            "apiVersion": "appcontroller.k8s/v1alpha1",
+            "kind": "Definition",
+            "metadata": {
+                "name": "job-%s" % name
+            },
+            "job": job
+        }
+    return job
 
 
 def serialize_deployment(name, spec, annotations, replicas, component_name,
@@ -404,6 +414,15 @@ def serialize_deployment(name, spec, annotations, replicas, component_name,
             }
         }
     }
+    if CONF.kubernetes.appcontroller.enabled:
+        deployment = {
+            "apiVersion": "appcontroller.k8s/v1alpha1",
+            "kind": "Definition",
+            "metadata": {
+                "name": "deployment-%s" % name
+            },
+            "deployment": deployment
+        }
 
     return deployment
 
@@ -513,6 +532,16 @@ def serialize_service(name, ports, headless=False, annotations=None):
     else:
         obj["spec"]["clusterIP"] = "None"
 
+    if CONF.kubernetes.appcontroller.enabled:
+        obj = {
+            "apiVersion": "appcontroller.k8s/v1alpha1",
+            "kind": "Definition",
+            "metadata": {
+                "name": "service-%s" % name
+            },
+            "service": obj
+        }
+
     return obj
 
 
@@ -556,4 +585,16 @@ def serialize_secret(name, type="Opaque", data={}):
         },
         "type": type,
         "data": data
+    }
+
+
+def serialize_dependency(name, parent, child):
+    return {
+        "apiVersion": "appcontroller.k8s/v1alpha1",
+        "kind": "Dependency",
+        "metadata": {
+            "name": name
+        },
+        "parent": parent,
+        "child": child
     }
