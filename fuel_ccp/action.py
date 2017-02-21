351d0fdb1ebaaf2955ae8e9ab513a83186c1441b
Andrey Pavlov <apavlov@mirantis.com>
2017-02-21 13:24:11 +0000

    Adding service-per-service support
    New config section introduced:
    
    services:
      keystone-db:
        service_def: mariadb
      keystone:
        service_def: keystone
        mapping:
          mariadb: keystone-db
    
    Defined services can be used in topology definition.
    In this example keystone-db service will be created from mariadb
    definition and keystone will use it instead of mariadb.
    
    Change-Id: I274826648390b844d240b7ae545c40264f662452

--- fuel_ccp/action.py
+++ fuel_ccp/action.py
@@ -56,9 +56,18 @@ class Action(object):
             self.user_parameters = user_parameters
         else:
             self.user_parameters = ()
+        self._process_dependencies()
         self._create_configmap()
         self._create_action()
         return self.k8s_name
+
+    def _process_dependencies(self):
+        services_map = utils.get_deploy_components_info()
+        deps_map = utils.get_dependencies_map(services_map)
+        new_deps = []
+        for dep in self.dependencies:
+            new_deps.extend(utils.extend_dependency(dep, deps_map, {}, {}))
+        self.dependencies = new_deps
 
     # configmap methods
 
